b010af0f65f7ccc3a678044799a9735e123ded07
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-05-31 09:50:02 -0400

    Summary2

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -3,22 +3,6 @@ import ReactModal from 'react-modal';
 import css from './ImageModal.module.css'
 import { Image } from '../App/types';
 
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//     backgroundColor: '#000', 
-//     border: 'none', 
-//     maxWidth: '90%', 
-//     maxHeight: '90%',
-//     overflow: 'auto', 
-//   },
-// };
-
 ReactModal.setAppElement('#root');
 
 interface ImageModalProps {
